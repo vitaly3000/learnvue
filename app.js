f41778ab90491cc677e1fe77df85ec73c09fd3e8
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2021-11-27 20:33:17 +0300

    lesson 2

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const App = {
-    data() {
-        return {
-            counter: 0,
-            title: 'Счетчик:',
-        }
-    }
-}
+  data() {
+    return {
+      counter: 0,
+      title: 'Список заметок:',
+      placeholder: 'Placeholder2',
+      inputValue: '',
+      notes: [],
+    };
+  },
+
+  methods: {
+    addNote() {
+      if (this.inputValue) {
+        this.notes.push(this.inputValue);
+        this.inputValue = '';
+      }
+    },
+
+    deleteNote(index, event) {
+      this.notes.splice(index, 1);
+    },
+  },
+  computed: {
+    double() {
+      console.log(22);
+      return this.notes.length;
+    },
+  },
+  watch: {
+    inputValue(value) {
+      console.log(value);
+    },
+  },
+};
 
 const app = Vue.createApp(App);
 
